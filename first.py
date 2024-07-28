cd5edc4ff374c35907e82b65e0017dddef9103c9
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-07-28 21:57:28 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,27 @@
+print("hello wrold")
+print("Mayank")
+print(24)
+print(18+24)
+
+#variables==
+name ="Mayank"
+age = 23
+price = 25.99
+age2 = age
+
+print("My name is : ", name)
+print("My age is : ", age)
+print("My price is : ", price)
+print("My age2 is : ", age2)
+# for finding datatype
+print (type(age2))
+print (type(name))
+print (type(price))
+print (type(age2))
+
+
+
+
+
+
+

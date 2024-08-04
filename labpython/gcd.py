2e3e3dc1f107b1ccf4bd88f4d427b38097a9665b
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-08-04 18:52:06 +0000

    gcd.py

--- labpython/gcd.py
+++ labpython/gcd.py
@@ -6,12 +6,8 @@ b = float(input("Enter the second number: "))
 a = int(a)
 b = int(b)
 
-# Ensure b is non-negative for the loop to work correctly
-if b < 0:
-    b = -b
-
 # Euclidean algorithm to find GCD
-while b > 0:
+while b != 0:
     r = a % b
     a = b
     b = r

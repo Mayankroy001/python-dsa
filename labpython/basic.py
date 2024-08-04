225cadd4aea3d2a76b2b9626ec83a987e9add39c
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2024-08-04 09:51:07 +0000

    basic.py

---
+++ labpython/basic.py
@@ -0,0 +1,38 @@
+#addition===========
+val1=int(input("enter your first number : "))
+val2=int(input("Enter your second  number : "))
+#print("Your answer : ",val1+val2)
+
+#substractionn===========
+val1=int(input("enter your first number : "))
+val2=int(input("Enter your second  number : "))
+#print("Your answer : ",val1-val2)
+
+#multiplication===========
+val1=int(input("enter your first number : "))
+val2=int(input("Enter your second  number : "))
+#print("Your answer : ",val1*val2)
+
+#division===========
+val1=float(input("enter your first number : "))
+val2=float(input("Enter your second  number : "))
+#print("Your answer : ",val1/val2)
+
+#modulas===========
+val1=float(input("enter your first number : "))
+val2=float(input("Enter your second  number : "))
+#print("Your answer : ",val1%val2)
+
+#exponent===========
+import math
+base=int(input("enter your base : "))
+exponent=int(input("Enter your exponent : "))
+s = pow(base,exponent)
+print("Your answer : ",s)
+
+#floor value=====
+
+import math
+value = float(input("Enter the value : "))
+floor_value = math.floor(value)
+print("Your result : ", floor_value)
